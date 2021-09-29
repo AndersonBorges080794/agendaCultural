51d512f76c2d36cbe16275f15600eddda2f3e5fd
Anderson Borges <andersonborges08@hotmail.com>
2021-09-29 11:07:46 -0300

    third commit

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
-const loginController = require('./src/controllers/loginController');
+const loginController = require('./src/controllers/LoginController');
 
 
 // Rotas da home
@@ -9,6 +9,9 @@ route.get('/', homeController.index);
 
 //Rotas de login
 route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout)
 
 
 module.exports = route;
